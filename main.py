796ad84856386e9f69762f8b8a3e67dd7de68ced
Zechw <zechwilliams@gmail.com>
2018-12-06 23:55:10 -0700

    WIP seven part two

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class Node:
 		self.id = node_id
 		self.parents = []
 		self.children = []
+		self.work_remaining = 60 + ord(node_id.upper()) - 64 
 	def add_parent(self, p_id):
 		if p_id not in self.parents:
 			self.parents.append(p_id)
@@ -19,6 +20,59 @@ class Step:
 		self.parent = r.group(1)
 		self.child = r.group(2)
 
+class WorkEngine:
+	def __init__(self, nodes, worker_count):
+		self.time = 0
+		self.nodes = nodes
+		self.available_nodes = []
+		for node in nodes.values():
+			if len(node.parents) == 0:
+				self.available_nodes.append(node.id)
+		self.workers = [None] * worker_count
+		self.path = []
+	def run(self):
+		while len(self.available_nodes) != 0 or self.workers != [None] * len(self.workers):
+			self.do_cycle()
+			print(self.workers)
+		print(self.time)
+		print(self.path)
+	def do_cycle(self):
+		self.time += 1
+		self.available_nodes.sort()
+		for i, work_id in enumerate(self.workers):
+			if work_id is None:
+				work_id = self.claim_next_node(i)
+			if work_id is not None:
+				self.do_work(i, work_id)
+	def do_work(self, worker_i, work_id):
+		self.nodes[work_id].work_remaining -= 1
+		if self.nodes[work_id].work_remaining == 0:
+			self.path.append(work_id)
+			for child_id in self.nodes[work_id].children:
+				self.nodes[child_id].parents.remove(work_id)
+				if len(self.nodes[child_id].parents) == 0:
+					self.available_nodes.append(child_id)
+			self.claim_next_node(worker_i)
+	def claim_next_node(self, worker_i):
+		node_id = None
+		if len(self.available_nodes) != 0:
+			node_id = self.available_nodes.pop(0)
+		self.workers[worker_i] = node_id
+		return node_id
+
+
+sample = ['Step C must be finished before step A can begin.',
+	'Step C must be finished before step F can begin.',
+	'Step A must be finished before step B can begin.',
+	'Step A must be finished before step D can begin.',
+	'Step B must be finished before step E can begin.',
+	'Step D must be finished before step E can begin.',
+	'Step F must be finished before step E can begin.']
+
+
+sample_2 = ['Step A must be finished before step B can begin',
+	'Step A must be finished before step C can begin',
+	'Step C must be finished before step D can begin']
 
 
 with open('7.txt') as file:
@@ -33,21 +87,10 @@ with open('7.txt') as file:
 		if step.child not in nodes:
 			nodes[step.child] = Node(step.child)
 		nodes[step.child].add_parent(step.parent)
-	
-	#process_steps
-	available_nodes = []
-	for node in nodes.values():
-		if len(node.parents) == 0:
-			available_nodes.append(node.id)
-	
-	path = []
-	while len(available_nodes) != 0:
-		available_nodes.sort()
-		node_id = available_nodes.pop(0)
-		path.append(node_id)
-		for child_id in nodes[node_id].children:
-			nodes[child_id].parents.remove(node_id)
-			if len(nodes[child_id].parents) == 0:
-				available_nodes.append(child_id)
-print(path)
+
+
+
+	e = WorkEngine(nodes, 5)
+	e.run()
+	exit()
 
